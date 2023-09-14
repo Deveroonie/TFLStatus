3e5c6a99a8ebe91b4ec2c824084f96b3ab0b5779
Deveroonie <sirepiclydev@gmail.com>
2023-09-14 16:26:50 +0100

    TramStatus.js

---
+++ src/components/TramStatus.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function TramStatus() {
+  const [response, setResponse] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("https://api.tfl.gov.uk/line/mode/tram/status");
+        setResponse(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const [expandedLine, setExpandedLine] = useState(null);
+
+  const handleLineClick = (lineId) => {
+    if (expandedLine === lineId) {
+      setExpandedLine(null);
+    } else {
+      setExpandedLine(lineId);
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-1 gap-4 text-white" style={{paddingTop: "16px"}}>
+      {response.map(data => (
+        <div key={data.id} className="p-4 bg-gray-800 rounded-lg">
+          <div
+            className="cursor-pointer font-bold mb-2 lnnm text-2xl flex"
+            onClick={() => handleLineClick(data.id)}
+          >
+            {data.name} - {data.lineStatuses[0].statusSeverityDescription}&nbsp;<img src="/trams.png" width={64} h={64} alt={"Trams Logo"}></img>
+          </div>
+          {expandedLine === data.id && (
+            <div className="mt-2 stnm text-lg">
+              <p>{data.lineStatuses[0].reason || "Good Service"}</p>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TramStatus;
